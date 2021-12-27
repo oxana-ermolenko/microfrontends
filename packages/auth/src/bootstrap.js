1cca333b996aa8073f2e6f3cb872b663052a45d9
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2021-12-26 23:31:59 -0800

    add auth functionality

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -6,7 +6,7 @@ import App from './App';
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   const history = defaultHistory || createMemoryHistory({
-    initialEntries: [initialPath],
+    initialEntries: [initialPath]
   });
   
   if (onNavigate) {
@@ -29,7 +29,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
